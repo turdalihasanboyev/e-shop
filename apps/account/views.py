1cfc08db4e2cd047b5bf90835089c418ab1d8c7b
Turdali <turdalihasanboyev144@gmail.com>
2025-06-15 19:22:09 +0500

    `Removed unused imports and messages from views and templates, simplified user creation in register_view.`

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render, redirect
 
-from django.contrib.auth import get_user_model
-
-from django.contrib import messages
-
+from django.contrib.auth.models import User
 from django.contrib.auth import logout, login, authenticate
-
 from django.contrib.auth.decorators import login_required
-
-User = get_user_model()
 
 
 @login_required
 def logout_view(request):
     logout(request)
-    messages.success(request, "Foydalanuvchi tizimdan muvaffaqiyatli chiqdi!")
     return redirect('home')
 
 def register_view(request):
@@ -27,22 +20,11 @@ def register_view(request):
         confirm_password = request.POST.get('confirm_password')
 
         if password != confirm_password:
-            messages.error(request, "Parollar bir xil emas!")
             return redirect('register')
-
         if User.objects.filter(username=username).exists():
-            messages.error(request, "Username allaqachon mavjud!")
             return redirect('register')
 
-        user = User.objects.create_user(
-            username=username,
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            password=password,
-        )
-        user.save()
-        messages.success(request, "Foydalanuchi muvaffaqiyatli ro'yhatdan o'tdi")
+        user = User.objects.create_user(username=username, email=email, first_name=first_name, last_name=last_name, password=password)
         login(request, user)
         return redirect('home')
 
@@ -52,15 +34,12 @@ def login_view(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
-
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
-            messages.success(request, "Foydalanuvchi muvaffaqiyatli tizimga kirdi!")
             return redirect('home')
         else:
-            messages.error(request, "Parol yoki username xato!")
             return redirect('login')
 
     return render(request, 'login.html')
